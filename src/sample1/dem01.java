56b4b4bf532686d595b9f1b0fcd209fc03714ecf
91885 <91885@LAPTOP-U631NH3F>
2022-10-15 14:21:42 +0530

    [Pooja] : created 3 class dem01, demo2 and demo3

---
+++ src/sample1/dem01.java
@@ -0,0 +1,18 @@
+package sample1;
+
+public class dem01 
+{
+	public static void main(String[] args)
+	{
+		//even-odd
+		int num=3;
+		if(num%2==0)
+		{
+			System.out.println("given num is even");
+		}
+		else
+		{
+		System.out.println("given num is odd");
+	    }
+	}
+}
---
+++ src/sample1/demo2.java
@@ -0,0 +1,18 @@
+package sample1;
+
+public class demo2 
+{
+	public static void main(String[] args)
+	{
+		//factorial
+		int num=3;
+		int fact=1;
+		
+		for(int i=num; i>0; i--)
+		{
+			fact=fact*i;		
+	    }
+		System.out.println(fact);
+	}
+
+}
